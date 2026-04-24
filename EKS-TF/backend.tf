5a7ed30f3c592f0765b5bf3e0b64b57689a44938
Shweta <shwetabhore18@gmail.com>
2026-04-24 12:06:35 +0530

    bucket name changes

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "abhi-project-mario" # Replace with your actual S3 bucket name
+    bucket = "mario-project-bucket-26" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
     profile = "eks"
